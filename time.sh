996bdbbe44af73dc1ce706a9e4eb8b2861dc6075
artnovak <artnovak1993@gmail.com>
2022-02-03 23:48:55 +1000

    added a command /bin/zsh in the end of script

--- time.sh
+++ time.sh
@@ -6,52 +6,52 @@ case $x in
         Mon)
                 echo "Today is Monday"
                 sleep 0.2
-                echo' ,----------------------------.               '
-                echo' |I invented a new word today:|      ...      '
-                echo' |        Plagiarism.         |     (o -)     '
-                echo' `----------------------------` ooO--(_)--Ooo-'
+                echo ',----------------------------.               '
+                echo '|I invented a new word today:|      ...      '
+                echo '|        Plagiarism.         |     (o -)     '
+                echo '`----------------------------` ooO--(_)--Ooo-'
         ;;
         Tue)
                 echo  "Today is Tuesday"
                 sleep 0.2
-                echo' ,---------------------------------.                '
-                echo' |   What did one traffic light    |       ...      '
-                echo' |       say to the other?         |(_    (o -)     '
-                echo' | Stop looking at me, Im changing!|  ooO--(_)--Ooo-'
-                echo' `---------------------------------`                '
+                echo ',---------------------------------.                '
+                echo '|   What did one traffic light    |       ...      '
+                echo '|       say to the other?         |(_    (o -)     '
+                echo '| Stop looking at me, Im changing!|  ooO--(_)--Ooo-'
+                echo '`---------------------------------`                '
         ;;
         Wed)
                 echo 'Today is Wednesday'
                 sleep 0.2
-                echo ' ,---------------------------------.               '
-                echo ' | Two artists had an art contest. |     ...       '
-                echo ' |-It ended in a draw!!            |    (o -)      '
-                echo ' `---------------------------------`ooO--(_)--Ooo- '
+                echo ',---------------------------------.               '
+                echo '| Two artists had an art contest. |     ...       '
+                echo '|-It ended in a draw!!            |    (o -)      '
+                echo '`---------------------------------`ooO--(_)--Ooo- '
         ;;
         Thu)
                 echo  "Today is Thursday"
                 sleep 0.2
-                echo' ,--------------------------------------.              '
-                echo' | What do you call bears with no ears? |     ...      '
-                echo' |    B.                                |    (o -)     '
-                echo' `------------------------------------- `ooO--(_)--Ooo-'
+                echo ',--------------------------------------.              '
+                echo '| What do you call bears with no ears? |     ...      '
+                echo '|    B.                                |    (o -)     '
+                echo '`------------------------------------- `ooO--(_)--Ooo-'
         ;;
         Fri)
                 echo  "Today is Friday"
                 sleep 0.2
-                echo ' ,------------------------------.               '
-                echo ' |I used to breed rabbits.  .   |      ...      '
-                echo ' | Then I realized              |     (o -)     '
-                echo ' |they can handle it themselves.| ooO--(_)--Ooo-'
-                echo ' `------------------------------`               '
+                echo ',------------------------------.               '
+                echo '|I used to breed rabbits.  .   |      ...      '
+                echo '| Then I realized              |     (o -)     '
+                echo '|they can handle it themselves.| ooO--(_)--Ooo-'
+                echo '`------------------------------`               '
         ;;
         Sut)
                 echo -n "Todat is Saturday"
                 sleep 0.2
-                echo' ,------------------------------.               ' 
-                echo' | What is Jesus favorite food? |      ...      '
-                echo' |        Cheeses.              |     (o -)     '
-                echo' `------------------------------` ooO--(_)--Ooo-'
+                echo ',------------------------------.               ' 
+                echo '| What is Jesus favorite food? |      ...      '
+                echo '|        Cheeses.              |     (o -)     '
+                echo '`------------------------------` ooO--(_)--Ooo-'
         ;;
         Sun)
                 echo -n "Today is Sunday"
@@ -90,3 +90,6 @@ if [[ $x5 = 2[0-3] ]]
 then
 echo "Do yoga, (you can play a little) and sleep"
 fi
+
+#open shell
+/bin/zsh
